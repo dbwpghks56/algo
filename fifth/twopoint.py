939a014833462cec857f154896df2c3f3a74fe43
wpghks <wpghks56@naver.com>
2023-04-17 14:06:07 +0900

    two pointer 같은 방향

---
+++ fifth/twopoint.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.stdin = open('fifth/twopoint.txt', 'rt')
+input = sys.stdin.readline
+
+n, r = map(int, input().split())
+
+A = list(map(int, input().split()))
+count = 0
+s = 0
+e = 0
+sum = 0
+while (s < n) :
+    if (sum == r):
+        count += 1
+        sum -= A[s]
+        s+=1
+    elif (sum > r or e == n):
+        sum -= A[s]
+        s+=1
+    else:
+        sum += A[e]
+        e+=1
+
+print(count)

481631c955765bf3ce83be1e602abc7dbd9d7ce0
wpghks <wpghks56@naver.com>
2023-05-12 14:06:29 +0900

    bottom up 추가

--- thirteen-inflearn/dppibo.py
+++ thirteen-inflearn/dppibo.py
@@ -8,3 +8,14 @@ def fipo(n):
         memo[n] = fipo(n-1) + fipo(n-2)
         
     return memo[n]
+
+memo2 = {1 : 1, 2 : 1}
+# bottom up 형식
+def fipo2(n):
+    if n == 1 or n == 2: # 베이스 케이스가 있기 때문에 이 조건문은 없어도 됨
+        return 1
+    
+    for i in range(n):
+        memo2[i] = memo2[i-1] + memo2[i-2]
+        
+    return memo2[n]

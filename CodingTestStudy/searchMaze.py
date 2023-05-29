78fb847b46e5f2b56c5a2978f2c8f954898b48fa
JaeHwan <wpghks56@naver.com>
2023-05-29 22:37:22 +0900

    미로탐색 해결

--- CodingTestStudy/searchMaze.py
+++ CodingTestStudy/searchMaze.py
@@ -10,41 +10,37 @@ visited = []
 
 for i in range(r):
     A[i] = list(input().rstrip())
-print(A)
-def bfs(grid):
-    shortestPathLen = 0
-    row = r
-    col = c
+
+def bfs(grid, sl):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
     
-    def bfs(x, y,sl):
-        dx = [-1,1,0,0]
-        dy = [0,0,-1,1]
-        grid[x][y] = "0"
-        queue = deque()
-        queue.append((x,y))
-        while queue:
-            cur_x, cur_y = queue.popleft()
-
-            for i in range(4):
-                next_x = cur_x + dx[i]
-                next_y = cur_y + dy[i]
-                
-                if 0 <= next_x and next_x < r and 0 <= next_y and next_y < c:
-                    if grid[next_x][next_y] == "1":
-                        sl += 1
-                        grid[next_x][next_y] = "0"
-                        queue.append((next_x, next_y))
-
+    queue = deque()
+    queue.append((0,0,1))
+    if grid[0][0] != "1" or grid[r-1][c-1] != "1":
         return sl
+    grid[0][0] = "0"
+    while queue:
+        cur_x, cur_y, cur_l = queue.popleft()
 
-    for i in range(r):
-        for j in range(c):
-            if grid[i][j] == "1":
-                shortestPathLen = bfs(i,j, shortestPathLen)
-                
-    print(grid)
-    return shortestPathLen
+        if cur_x == r-1 and cur_y == c-1:
+            sl = cur_l
+            break
+
+        for i in range(4):
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            
+            if 0 <= next_x and next_x < r and 0 <= next_y and next_y < c:
+                if grid[next_x][next_y] == "1":
+                    grid[next_x][next_y] = "0"
+                    queue.append((next_x, next_y, cur_l+1))
+
+            if next_x == r and next_y == c:
+                return sl
+
+    return sl
 
 
 
-print(bfs(A))
+print(bfs(A, 1))
